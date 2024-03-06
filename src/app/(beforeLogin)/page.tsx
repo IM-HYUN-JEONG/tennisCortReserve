e81a2ff3a3d6796f9f52f804c82c8751faedb363
ImHyunJeong <fltxld3@naver.com>
2024-03-06 17:53:30 +0900

    feat: setting

---
+++ src/app/(beforeLogin)/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from "next/navigation";
+import Main from "./_component/Main";
+
+export default async function Home() {
+  //  if (session?.user) {
+  //  redirect('/home');
+  //  return null;
+  // }
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
