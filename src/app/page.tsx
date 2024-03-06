2f8062926325688125f595617dfb32ed3e813fe4
Hyun <95120267+LIM-HYUN-JEONG@users.noreply.github.com>
2024-03-06 22:45:07 +0900

    feat: add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Metadata } from "next";
 
+export const metadata:Metadata = {
+  title: 'Tennis Cort Reserve | Home',
+  description: 'Tennis Cort Reserve | Home',
+}
 export default function Home() {
   return (
     <main >
