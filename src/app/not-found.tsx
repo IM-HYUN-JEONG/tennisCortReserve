9418556dd3da406dbda3f7ae90585e1fa0979a24
ImHyunJeong <fltxld3@naver.com>
2024-03-07 18:02:59 +0900

    feat: Edit pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,17 @@
+"use client";
 import Link from "next/link";
 import {NextPage} from "next";
+import styled from "styled-components";
 
 const NotFound: NextPage = () => {
   return (
-    <div>
+    <Container>
       <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</div>
-      <Link href="/">Home</Link>
-    </div>
-  )
-}
+      <Link href="/">홈으로</Link>
+    </Container>
+  );
+};
 
 export default NotFound;
+
+const Container = styled.div``;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import {Metadata} from "next";
+import {BrowserView, MobileView, isDesktop, isTablet, isMobileOnly} from "react-device-detect";
+import styled from "styled-components";
 
 export const metadata: Metadata = {
   title: "Tennis Cort Reserve | Home",
   description: "Tennis Cort Reserve | Home",
 };
+
 export default function Home() {
-  return <main style={{}}>Home</main>;
+  return <main>home</main>;
 }
